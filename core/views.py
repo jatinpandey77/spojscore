59695f80c015540f73a1451e6e1fb6ca805085c2
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2018-06-04 04:42:21 +0530

    updated core.views.py

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Problem
 
 def index(request):
-    problems = Problem.objects.all()
+    problems = Problem.objects.all().order_by('-users')
     return render(request, 'core/core.html', {'problems': problems})
