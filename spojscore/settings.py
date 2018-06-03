3f9c52d5c00df54f7b44fb58647df1cce2e434bc
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2018-06-04 05:13:48 +0530

    updated settings.py

--- spojscore/settings.py
+++ spojscore/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 DATABASES = {
     'default': dj_database_url.config(
