6fc2a8f19e3e408262d6e2b438615eea527fdf6a
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2018-06-04 05:05:15 +0530

    updated settings.py
    added 'spojscore.alfarhanzahedi.com' to ALLOWED_HOSTS

--- spojscore/settings.py
+++ spojscore/settings.py
@@ -5,7 +5,9 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = config('SECRET_KEY')
+
 DEBUG = config('DEBUG', default=True, cast=bool)
+
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
