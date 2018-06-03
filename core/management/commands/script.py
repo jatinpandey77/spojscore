0df439c89e54540645cada340090631ea3ede0ad
Alfarhan Zahedi <alfarhanzahedi@gmail.com>
2018-06-04 03:07:20 +0530

    updated script.py with update_or_create()

--- core/management/commands/script.py
+++ core/management/commands/script.py
@@ -1,18 +1,19 @@
 from django.core.management.base import BaseCommand
 from core.models import Problem
 import bs4, requests
+import datetime
 
 
 class Command(BaseCommand):
     help = 'Scrapes spoj.com to obtain the details of all the listed problems on the aforementioned website.'
     def handle(self, *args, **options):
+        self.stdout.write('\nScraping started at %s\n' % str(datetime.datetime.now()))
         count = 0
         problem_name = ''
         problem_code = ''
         problem_score = 0.0
         problem_users = 0
         while count != 3650:
-            self.stdout.write("Extracting details for problems " + str(count + 1) + " to " + str(count + 50))
             url = "http://www.spoj.com/problems/classical/sort=6,start=" + str(count)
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36', 'cookie':'inweb_city=Kolkata;'}
             res = requests.get(url, headers = headers)
@@ -25,6 +26,6 @@ class Command(BaseCommand):
                 problem_code = elements_one["href"].split("/")[-1]
                 problem_score = float(elements_two["title"].split(" ")[1])
                 problem_users = int(elements_two.getText())
-                temp = Problem(name=problem_name, code=problem_code, users=problem_users, score=problem_score)
-                temp.save()
+                obj, created = Problem.objects.update_or_create(code=problem_code, defaults={'name':problem_name, 'code':problem_code, 'users':problem_users, 'score':problem_score})
             count += 50
+        self.stdout.write('\nScraping ended at %s\n' % str(datetime.datetime.now()))
